77832c29c19d56c3faa939db0462f5040016f08c
president <abanda435@gmail.com>
2022-05-13 11:54:53 +0200

    Seeder file for listings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,17 +16,6 @@ class DatabaseSeeder extends Seeder
     {
          \App\Models\User::factory(5)->create();
 
-         Listing::create([
-             'title' => 'Laravel Senior Developer',
-             'tags' => 'laravel, javascript',
-             'company_name' => 'Chiweto',
-             'location' => 'Lilongwe',
-             'email' => 'albert@chiweto.ch',
-             'website' => 'www.chiweto.ch',
-             'description' => 'Failed! Error: SET PASSWORD has no significance for user 
-             \'root\'@\'localhost\' as the authentication method used doesn\'t store 
-             authentication data in the MySQL server. Please consider using ALTER USER 
-             instead if you want to change authentication parameters.'
-         ]);
+         Listing::factory(1)->create();
     }
 }
