7efef2b5ee7ea72d072da131f008e405736602b7
president <abanda435@gmail.com>
2022-08-07 23:36:38 +0200

    adding slots for bg effects

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,7 +1,6 @@
 @props(['listing'])
 
 <!-- Item 1 -->
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
