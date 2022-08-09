768f392c48f01befb789c324dfb13485463c2a36
president <abanda435@gmail.com>
2022-08-10 00:02:48 +0200

    re ordering listing tags to be dynamic

--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -1,24 +1,14 @@
 @props(['tagsCSV')
 
+@php
+    $tags=explode(',',$tagsCSV]);
+@endphp
 <ul class="flex">
+    @foreach($tags as $tag)
     <li
         class="bg-black text-white rounded-xl px-3 py-1 mr-2"
     >
-        <a href="#">Laravel</a>
+        <a href="#">$tag</a>
     </li>
-    <li
-        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-    >
-        <a href="#">API</a>
-    </li>
-    <li
-        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-    >
-        <a href="#">Backend</a>
-    </li>
-    <li
-        class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-    >
-        <a href="#">Vue</a>
-    </li>
+    @endforeach
 </ul>
