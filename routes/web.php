a640dbcca771ec4be3c5d7a17cf0eb1c6ef1ed36
president <abanda435@gmail.com>
2022-08-14 23:50:55 +0200

    cleaned we routes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('providers', [
-        'heading' => 'Ready Gigs',
         'listings' => Listing::all(),
     ]);
 });
